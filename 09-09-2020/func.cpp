75ddb5f6c7600fb96fd9d6c70b68d0f234d59feb
Fabian Rosero <froseror@unal.edu.co>
2020-09-09 12:18:44 -0500

    new file:   09-09-2020/func.cpp

---
+++ 09-09-2020/func.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void print_message(int a)
+{
+  std::cout << "Hello world!\n";
+  std::cout << "Parámetro a: " << a << "\n";
+}
+
+int main(void)
+{
+  int m = 12;
+  print_message(m);
+  return 0;
+}
---
+++ 09-09-2020/func2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+double average(double a, double b); 
+
+int main (void)
+{
+  double x = 9.8, y = 10.76, z = 0;
+  z = average(x, y);
+  std::cout << z << std::endl;
+  return 0;
+}
+
+double average(double a, double b)
+{
+  double average = (a+b)/2;
+  return average;
+}
+  
